356b0a0d611a68b3e0e32518f70133c9e3ba372e
Tunahan Gediz <tgediz19@posta.pau.edu.tr>
2023-03-10 12:08:27 +0300

    tunahangedi.com added to projects list

--- components/Projects.js
+++ components/Projects.js
@@ -29,7 +29,7 @@ function Projects() {
                     <Link
                       href={project.githubUrl}
                       target="_blank"
-                      className="flex items-center hover:text-2xl transition-all duration-200 py-2 px-4 rounded-lg dark:hover:bg-[#484f8f]  bg-[#eef0ff] dark:bg-[#d3d8fb] text-[#484f8f] font-medium"
+                      className="flex items-center hover:text-2xl transition-all duration-200 py-2 px-4 rounded-lg bg-[#eef0ff] dark:bg-[#d3d8fb] text-[#484f8f] font-medium"
                     >
                       Github <i class="ri-github-fill ml-1 text-[#484f8f] "></i>
                     </Link>
@@ -38,7 +38,7 @@ function Projects() {
                     <Link
                       href={project.websiteUrl}
                       target="_blank"
-                      className="flex items-center ml-4 py-2 px-4  hover:text-2xl transition-all duration-200 rounded-lg dark:hover:bg-[#484f8f]  bg-[#eef0ff] dark:bg-[#d3d8fb] text-[#484f8f] font-medium"
+                      className="flex items-center ml-4 py-2 px-4  hover:text-2xl transition-all duration-200 rounded-lg  bg-[#eef0ff] dark:bg-[#d3d8fb] text-[#484f8f] font-medium"
                     >
                       View Website <i class="ri-external-link-fill ml-1 "></i>
                     </Link>
